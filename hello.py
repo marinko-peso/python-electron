d0ca729850e8c68c1c8944e486bd09bf9d39bdcb
marinko <marinko.peso@gmail.com>
2018-09-02 19:47:18 +0200

    initial python communication

---
+++ hello.py
@@ -0,0 +1,4 @@
+import sys
+
+print('This is from Python!')
+sys.stdout.flush()
--- main.js
+++ main.js
@@ -3,6 +3,11 @@ const { app, BrowserWindow } = require('electron');
 function createWindow() {
     window = new BrowserWindow({ width: 800, height: 600 });
     window.loadFile('index.html');
+
+    const python = require('child_process').spawn('python', ['./hello.py']);
+    python.stdout.on('data', data => {
+        console.log('data: ', data.toString('utf8'));
+    });
 }
 
 app.on('ready', createWindow);
